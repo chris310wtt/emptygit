d993f2eaa2ae52220945b33ec6301606d978b2a4
chris <1120523509@qq.com>
2018-06-08 16:26:32 +0800

    自动生成版本3

--- example-app/example_app/msg/api/get_msg.py
+++ example-app/example_app/msg/api/get_msg.py
@@ -6,10 +6,36 @@ from flask import request, g
 from . import Resource
 from .. import schemas
 
+from . import Resource
+
+import connexion
+
+import json
+
+import pymysql.cursors
+
+app = connexion.App(__name__)
 
 class GetMsg(Resource):
 
-    def get(self):
+    def get(Product_Line_Id=None):
         print(g.args)
-
-        return {}, 200, None
+        conn = pymysql.connect(host='192.168.1.254', user='root', password='111111', database='testwutt',
+                               charset='utf8')
+        cursor = conn.cursor()
+        sql_select = '''select  MESSAGE_NAME,
+                                            MESSAGE_CONTENT
+                                            from SYSTEM_MESSAGE
+                                            # where PRODUCT_LINE_ID = % s ''' % (Product_Line_Id)
+        print(sql_select)
+        cursor.execute(sql_select)
+        values = cursor.fetchall()
+        print(values)
+        print(type(values))
+        # 字段含中文转成json串解决办法ensure_ascii=False
+        j = json.dumps(values, ensure_ascii=False)
+        # 关闭游标连接
+        cursor.close()
+        # 关闭数据库连接
+        conn.close()
+        return {"系统信息": j}, 200, None
