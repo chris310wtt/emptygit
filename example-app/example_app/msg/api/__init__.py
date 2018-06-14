7c7f634a08fba00388b45311a422c4207665d580
chris <1120523509@qq.com>
2018-06-14 10:32:15 +0800

    实战示例3webapp

--- example-app/example_app/msg/api/__init__.py
+++ example-app/example_app/msg/api/__init__.py
@@ -8,3 +8,23 @@ from ..validators import request_validate, response_filter
 
 class Resource(restful.Resource):
     method_decorators = [request_validate, response_filter]
+
+
+
+# # -*- coding: utf-8 -*-
+# from __future__ import absolute_import
+#
+# import msg
+#
+# from flask import Flask
+#
+#
+# def create_app():
+#     app = Flask(__name__, static_folder='static')
+#     app.register_blueprint(
+#         msg.bp,
+#         url_prefix='/msg')
+#     return app
+#
+# if __name__ == '__main__':
+#     create_app().run(debug=True)
